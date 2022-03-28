c37a4dd85ca8cc23d9a67fde4271fc61da2ab5cb
akshayc27 <37244257+akshayc27@users.noreply.github.com>
2022-03-28 09:16:31 +0530

    Delete app.py

--- WheatHeadDetection/app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st 
-#from PIL import Image
-#from classify import predict
-
-
-st.title("Upload + Classification Example")
-
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
